92dea59be9c873116175f3683f6b70c1b0079d81
du khon <32060434+benzi96@users.noreply.github.com>
2017-11-04 03:23:57 +0700

    version 2.3
    fix bug
    tinh cong no tai thoi diem
    tinh so tien phai tra tai thoi diem
    lam phieu tra tien cho nxb
    thanh toan so tien no cua dai ly

--- ctyppsachmvc/Controllers/tonkhoesController.cs
+++ ctyppsachmvc/Controllers/tonkhoesController.cs
@@ -15,27 +15,34 @@ namespace ctyppsachmvc.Controllers
         private ctyppsachEntities db = new ctyppsachEntities();
 
         // GET: tonkhoes
-        public ActionResult Index(string thoidiem, int idsach)
+        public ActionResult Index(string thoidiem, string ids)
         {
-
+            ViewBag.idsach = new SelectList(db.sach, "idsach", "tensach");
             DateTime searchDate;
-
+            int idsach = 0;
             if (DateTime.TryParse(thoidiem, out searchDate))
             {
-                List<sach> sach = new List<sach>();
-                var ctpn = db.ctpn.Where(ct => ct.idsach == idsach && ct.phieunhap.ngaynhap > searchDate);
-                int soluongnhap = (int)ctpn.Sum(ct => ct.soluong);
-                var ctpx = db.ctpx.Where(ct => ct.idsach == idsach && ct.phieuxuat.ngayxuat > searchDate);
-                int soluongxuat = (int)ctpx.Sum(ct => ct.soluong);
-                int soluongtonhientai = (int)db.sach.Find(idsach).soluongton;
-                int soluongtonquakhu = soluongtonhientai + soluongxuat - soluongnhap;
+                List<sach> saches = new List<sach>();
+                if (int.TryParse(ids, out idsach))
+                    saches = db.sach.Where(o => o.idsach == idsach).Include(t => t.nxb).ToList();
+                else saches = db.sach.Include(t => t.nxb).ToList();
 
-                sach a = db.sach.Find(idsach);
-                sach.Add(a);
-                return View(sach);
-                // do not use .Equals() which can not be converted to SQL
+                foreach (sach o in saches)
+                {
+                    int soluongnhap = (int)db.ctpn.Where(ct => ct.idsach == o.idsach && ct.phieunhap.ngaynhap > searchDate)
+                                                  .Select(ct => ct.soluong)
+                                                  .DefaultIfEmpty(0)
+                                                  .Sum();
+                    int soluongxuat = (int)db.ctpx.Where(ct => ct.idsach == o.idsach && ct.phieuxuat.ngayxuat > searchDate)
+                                                  .Select(ct => ct.soluong)
+                                                  .DefaultIfEmpty(0)
+                                                  .Sum();
+
+                    o.soluongton = o.soluongton + soluongxuat - soluongnhap;
+                }
+                return View(saches);
             }
-
+            
             var s = db.sach.Include(t => t.nxb);
             return View(s.ToList());
         }
